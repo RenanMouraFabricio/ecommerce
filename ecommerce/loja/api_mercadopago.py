941fd524f103b5fa527f20a06faca7b018f264bc
RenanMouraFabricio <renan.fabricio@outlook.com.br>
2024-05-06 17:31:32 -0300

    first commit

---
+++ ecommerce/loja/api_mercadopago.py
@@ -0,0 +1,36 @@
+import mercadopago
+
+public_key = "TEST-88da63f7-edd0-469c-9b7c-214f190dfb23"
+token = "TEST-7738534265674492-050112-8a67fbcb7f1eba65e8375c5282a95d78-124585707"
+
+def criar_pagamento(itens_pedido, link):
+    # Configure as credenciais
+    sdk = mercadopago.SDK(token)
+    # Crie um item na preferência
+
+    # itens que ele está comprando no formato de dicionário
+    itens = []
+    for item in itens_pedido:
+        quantidade = int(item.quantidade)
+        nome_produto = item.item_estoque.produto.nome
+        preco_unitario = float(item.item_estoque.produto.preco)
+        itens.append({
+            "title": nome_produto,
+            "quantity": quantidade,
+            "unit_price": preco_unitario,
+        })
+
+    # valor total
+    preference_data = {
+        "items": itens,
+        "auto_return": "all",
+        "back_urls": {
+            "success": link,
+            "pending": link,
+            "failure": link,
+        }
+    }
+    resposta = sdk.preference().create(preference_data)
+    link_pagamento = resposta["response"]["init_point"]
+    id_pagamento = resposta["response"]["id"]
+    return link_pagamento, id_pagamento
